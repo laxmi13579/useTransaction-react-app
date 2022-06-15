b3fd5abbb596f80066587996926afc859c485e95
laxmi13579 <laxmi.yadav370515@gmail.com>
2022-06-15 20:29:33 +0545

    set priority high to input and low to list of data to be display in UI using useTransaction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useTransition } from 'react';
 
 function App() {
+  const [text, setText] = useState();
+  const [list, setList] = useState([]);
+  const [isPending, startTransition] = useTransition();
+  const LIST_SIZE = 20000;
+  
+
+  function textHandler(e){
+    setText(e.target.value);
+    //it will set low priority to the code in startTransaction, it will render twice if the value is changed one for - setText(e.target.value); and another for code in startTransaction
+    startTransition(()=>{
+      const l = []
+      for ( let i = 0; i<= LIST_SIZE; i++){
+        l.push(e.target.value)
+      }
+      setList(l);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type='text' value={text} onChange={textHandler}/>
+      {isPending ? 'Loading....': list.map((item,index)=>{ return <div key={index}>{item}</div>})}
+    </>
   );
 }
 
